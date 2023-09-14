c8f81f3c7e29044e9cfe5207d5196e6aa98ac68d
KristhianCacho <kristhiancacho518@gmail.com>
2023-09-14 11:40:17 -0700

    10-add.py

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
-
 def add(a, b):
-
-    # Add validations just for fun and learn, exercise expect error
-    checkA = isinstance(a, int)
-    checkB = isinstance(b, int)
-
-    if (checkA and checkB) == 1:
-        return(a + b)
-    else:
-        return(a + b)
+    return (a + b)
