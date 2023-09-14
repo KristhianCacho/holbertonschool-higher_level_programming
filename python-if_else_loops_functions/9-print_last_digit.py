ca38bd949de1baf80e31cecf3a10cc4c877beb1d
KristhianCacho <kristhiancacho518@gmail.com>
2023-09-14 11:38:21 -0700

    9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
-
 def print_last_digit(number):
-
-    check = abs(number)
-    check %= 10
-
-    if check >= 0 and check <= 9:
-        print("{:d}".format(check), end="")
-        return(check)
-    else:
-        pass
+    if number < 0:
+        number = -number
+    r = number % 10
+    print("{:d}".format(r), end="")
+    return(r)
