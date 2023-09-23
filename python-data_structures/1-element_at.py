d6224e89a6885404a3dd57bb8f13abca02643716
KristhianCacho <kristhiancacho518@gmail.com>
2023-09-22 19:56:15 -0700

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+
+
 def element_at(my_list, idx):
+    """retrieves an element from a list like in C
+    """
+
     if idx < 0:
-        return (None)
-    length = len(my_list)
-    if idx > length - 1:
-        return (None)
-    return(my_list[idx])
+        return None
+    elif idx > (len(my_list) - 1):
+        return None
+    else:
+        return(my_list[idx])
